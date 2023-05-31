850935b01c853870275d546c450fa9284f090fb3
kadzhaeva-nataliya <08112007@mail.ru>
2023-06-01 02:35:05 +0300

    fix: Swiper Slider

--- js/main.js
+++ js/main.js
@@ -35,3 +35,32 @@ mMenuToggle.addEventListener("click",(event) => {             /*вешаем н�
     event.preventDefault();               /*не реагировать на стандартный клик*/
     menu.classList.contains("is-open")? closeMenu() : openMenu();       
 });
+const swiper = new Swiper(".swiper", {
+    speed: 400,
+    autoHeight: true,   
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".slider-button-next",
+        prevEl: ".slider-button-prev",
+     },
+     breakpoints: {
+        // when window width is >= 576px
+        576: {
+          slidesPerView: 2,
+          centeredSlides: true,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,        
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,        
+        },
+        // when window width is >= 1200px
+        1200: {
+            slidesPerView: 5,        
+          },
+      }
+});
+ 
