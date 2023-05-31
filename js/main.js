26042b99f806871718d5f0e5054073656a5d94a1
kadzhaeva-nataliya <08112007@mail.ru>
2023-05-31 12:10:47 +0300

    fix: adaptive header

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle"); /*Кнопка, которая вкл меню*/
+const menu = document.querySelector(".mobile-menu"); /*сама кнопка*/
 window.addEventListener ("scroll", () => {
     if (this.scrollY > 1) {
     navbar.classList.add ("navbar-light");
@@ -9,4 +11,8 @@ window.addEventListener ("scroll", () => {
     logo.href.baseVal = "img/sprite.svg#logo-light" 
     }
     
+});
+mMenuToggle.addEventListener("click",(event) => {             /*отслеживание события клик*/
+    event.preventDefault();               /*не реагировать на стандартный клик*/
+    menu.classList.toggle("is-open");         /*а найти меню и в его списке классов присвоить класс is-open*/
 });
