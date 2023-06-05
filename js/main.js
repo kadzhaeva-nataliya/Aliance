56cb0dd98d1d97352180b44f306e877f86412fa0
kadzhaeva-nataliya <08112007@mail.ru>
2023-06-06 02:03:04 +0300

    fix: Slider working scheme

--- js/main.js
+++ js/main.js
@@ -35,7 +35,33 @@ mMenuToggle.addEventListener("click",(event) => {             /*вешаем н�
     event.preventDefault();               /*не реагировать на стандартный клик*/
     menu.classList.contains("is-open")? closeMenu() : openMenu();       
 });
-const swiper = new Swiper(".swiper", {
+
+const swiperSteps = new Swiper (".steps-slider", {
+    speed: 400,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: ".steps-button-next",
+        prevEl: ".steps-button-prev",
+     },
+    breakpoints: {
+        // when window width is >= 576px
+        576: {
+          slidesPerView: 2,
+          centeredSlides: true,
+        },
+        // when window width is >= 768px
+        768: {
+          slidesPerView: 3,        
+        },
+        // when window width is >= 1024px
+        1024: {
+          slidesPerView: 4,        
+        },
+
+      }
+});
+
+const swiper = new Swiper(".features-slider", {
     speed: 400,
     autoHeight: true,   
     slidesPerView: 1,
