264ffeed820a967571301f40f47d281767fbe717
kadzhaeva-nataliya <08112007@mail.ru>
2023-06-19 13:30:13 +0300

    fix: php footer-cta data

--- index.php
+++ index.php
@@ -670,7 +670,7 @@
         <div class="footer-wrapper">
           <div class="footer-legal">
             <p class="footer-copyright">
-              &copy; 2022 «Aliance Production». Все права защищены.
+              &copy; <? echo date('Y')?> «Aliance Production». Все права защищены.
             </p>
             <a href="#" class="footer-policy">Политики конфиденциальности</a>
           </div>
